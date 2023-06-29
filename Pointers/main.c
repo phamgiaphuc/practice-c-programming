0588d624002d716c1a33ca4e30e9db36153dda28
Acus <acus1304pgphcm@gmail.com>
2023-06-30 01:10:22 +0700

    Phuc | Add | Lecture 8: Characters and Strings

--- Pointers/main.c
+++ Pointers/main.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-int main() {
-     int *array = (int*) malloc(10 * sizeof(int));
-     printf("%d\n", *array);
-     printf("%lu\n", sizeof(array));
-     return 0;
-}
+// Your code goes here...
